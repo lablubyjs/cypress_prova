063f9a5ab344e0b557cddb43118eeb3858ae1bb1
Crislâine Santos <criis.santos@outlook.com>
2022-04-03 09:08:25 -0300

    feat: test to create user added

--- cypress/integration/tgl.spec.js
+++ cypress/integration/tgl.spec.js
@@ -2,5 +2,18 @@ import '../support/commands'
 
 describe('TGL', () => {
 
-   
+   context('Registration', () => {
+      it('create user', () => {
+
+         cy.visit('/registration')
+
+         cy.get(':nth-child(1) > input').type('Cris')
+         cy.get(':nth-child(2) > input').type('cris@email.com')
+         cy.get(':nth-child(3) > input').type(123456)
+
+         cy.get('.sc-idiyUo > .sc-eCYdqJ').click()
+
+      });
+   })
+
 })
