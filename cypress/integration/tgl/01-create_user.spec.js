f42bdd6b3152f5c14e2fd8fde79ca97791c68248
Crislâine Santos <criis.santos@outlook.com>
2022-04-05 10:38:42 -0300

    fix: rearrangement of user creation test

---
+++ cypress/integration/tgl/01-create_user.spec.js
@@ -0,0 +1,16 @@
+describe('TGL', () => {
+
+   context('Registration', () => {
+      it('create user', () => {
+
+         cy.visit('/registration')
+
+         cy.get(':nth-child(1) > input').type('Cris')
+         cy.get(':nth-child(2) > input').type('cris@email.com')
+         cy.get(':nth-child(3) > input').type(123456)
+
+         cy.get('.sc-idiyUo > .sc-eCYdqJ').click()
+
+      });
+   })
+})
